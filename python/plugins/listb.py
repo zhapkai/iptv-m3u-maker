fc3d122ba54481880a383830df37955ccecc57c6
zhapkai <zhapkai@gmail.com>
2025-08-05 11:55:15 +0800

    Update listb.py

--- python/plugins/listb.py
+++ python/plugins/listb.py
@@ -6,15 +6,15 @@ import time
 import re
 import db
 import threading
+import os
 
-class Source (object) :
-
-    def __init__ (self):
+class Source(object):
+    def __init__(self):
         self.T = tools.Tools()
         self.now = int(time.time() * 1000)
         self.siteUrl = str('http://m.iptv807.com/')
 
-    def getSource (self) :
+    def getSource(self):
         urlList = []
 
         url = self.siteUrl
@@ -23,35 +23,35 @@ class Source (object) :
         ]
         res = self.T.getPage(url, req)
 
-        if res['code'] == 200 :
-            pattern = re.compile(r"<li><a href=\"(.*?)\" data-ajax=\"false\">.*?<\/a><\/li>", re.I|re.S)
+        if res['code'] == 200:
+            pattern = re.compile(r"<li><a href=\"(.*?)\" data-ajax=\"false\">.*?</a></li>", re.I|re.S)
             postList = pattern.findall(res['body'])
 
-            for post in postList :
+            for post in postList:
                 url = self.siteUrl + post
                 req = [
                     'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
                 ]
                 res = self.T.getPage(url, req)
 
-                if res['code'] == 200 :
+                if res['code'] == 200:
                     pattern = re.compile(r"<li><a href=\"(.*?)\" data-ajax=\"false\">(.*?)<\/a><\/li>", re.I|re.S)
                     channelList = pattern.findall(res['body'])
                     threads = []
 
-                    for channel in channelList :
+                    for channel in channelList:
                         channelUrl = self.siteUrl + channel[0]
-                        thread = threading.Thread(target = self.detectData, args = (channel[1], channelUrl, ), daemon = True)
+                        thread = threading.Thread(target=self.detectData, args=(channel[1], channelUrl,), daemon=True)
                         thread.start()
                         threads.append(thread)
 
                     for t in threads:
                         t.join()
 
-                else :
-                    pass # MAYBE later :P
+                else:
+                    self.T.logger(f"Failed to access {url}, code: {res['code']}")
 
-    def detectData (self, title, url) :
+    def detectData(self, title, url):
         req = [
             'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
         ]
@@ -59,55 +59,64 @@ class Source (object) :
         info = self.T.fmtTitle(title)
 
         playInfo = self.T.getPage(url, req)
-        pattern = re.compile(r"<option value=\"(.*?)\">.*?<\/option>", re.I|re.S)
+        if playInfo['code'] != 200:
+            self.T.logger(f"Failed to access {url}, code: {playInfo['code']}")
+            return
+
+        # 提取加密的 playUrl
+        pattern = re.compile(r"<option value=\"(.*?)\">.*?</option>", re.I|re.S)
         playUrlList = pattern.findall(playInfo['body'])
 
-        if len(playUrlList) > 0 :
+        if len(playUrlList) > 0:
             playUrl = playUrlList[0]
-            midM3uInfo = self.T.getPage(playUrl, req)
+            # 保存加密数据到文件
+            with open('encrypted_urls.txt', 'a', encoding='utf-8') as f:
+                f.write(f"{playUrl}\n")
+            self.T.logger(f"Saved encrypted URL for {info['title']}: {playUrl}")
 
-            pattern = re.compile(r"url: '(.*?)',", re.I|re.S)
-            midM3uUrlList = pattern.findall(midM3uInfo['body'])
-            if len(midM3uUrlList) > 0 :
-                midM3uUrl = midM3uUrlList[0]
+            # 注释掉后续处理，等待解密
+            # midM3uInfo = self.T.getPage(playUrl, req)
+            # pattern = re.compile(r"url: '(.*?)',", re.I|re.S)
+            # midM3uUrlList = pattern.findall(midM3uInfo['body'])
+            # if len(midM3uUrlList) > 0:
+            #     midM3uUrl = midM3uUrlList[0]
+            #     if midM3uUrl != '':
+            #         m3u = self.T.getRealUrl(midM3uUrl)
+            #         try:
+            #             m3u.index('migu.php?token=')
+            #         except:
+            #             if m3u != '':
+            #                 netstat = self.T.chkPlayable(m3u)
+            #             else:
+            #                 netstat = 0
+            #             if netstat > 0:
+            #                 cros = 1 if self.T.chkCros(m3u) else 0
+            #                 data = {
+            #                     'title': str(info['id']) if info['id'] != '' else str(info['title']),
+            #                     'url': str(m3u),
+            #                     'quality': str(info['quality']),
+            #                     'delay': netstat,
+            #                     'level': str(info['level']),
+            #                     'cros': cros,
+            #                     'online': 1,
+            #                     'udTime': self.now,
+            #                 }
+            #                 self.addData(data)
+            #                 self.T.logger('正在分析[ %s ]: %s' % (str(info['id']) + str(info['title']), m3u))
+            #             else:
+            #                 pass # MAYBE later :P
 
-                if midM3uUrl != '' :
-                    m3u = self.T.getRealUrl(midM3uUrl)
-
-                    try :
-                        m3u.index('migu.php?token=')
-                    except :
-                        if m3u != '' :
-                            netstat = self.T.chkPlayable(m3u)
-                        else :
-                            netstat = 0
-
-                        if netstat > 0 :
-                            cros = 1 if self.T.chkCros(m3u) else 0
-                            data = {
-                                'title'  : str(info['id']) if info['id'] != '' else str(info['title']),
-                                'url'    : str(m3u),
-                                'quality': str(info['quality']),
-                                'delay'  : netstat,
-                                'level'  : str(info['level']),
-                                'cros'   : cros,
-                                'online' : 1,
-                                'udTime' : self.now,
-                            }
-                            self.addData(data)
-                            self.T.logger('正在分析[ %s ]: %s' % (str(info['id']) + str(info['title']), m3u))
-                        else :
-                            pass # MAYBE later :P
-
-
-    def addData (self, data) :
+    def addData(self, data):
         DB = db.DataBase()
         sql = "SELECT * FROM %s WHERE url = '%s'" % (DB.table, data['url'])
         result = DB.query(sql)
 
-        if len(result) == 0 :
+        if len(result) == 0:
             data['enable'] = 1
             DB.insert(data)
-        else :
+        else:
             id = result[0][0]
             DB.edit(id, data)
+
+if __name__ == '__main__':
+    Source().getSource()
